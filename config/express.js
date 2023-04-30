b7a599b708050cddc659569394a44bee548c90ae
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-04-30 22:32:39 +0300

    Update express configuration

--- config/express.js
+++ config/express.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
-const cookieParser=require('cookie-parser');
+const cookieParser = require('cookie-parser');
+const session = require('../middlewares/session');
+const trimBody = require('../middlewares/trimBody');
 
 module.exports = (app) => {
     const hbs = handlebars.create({
@@ -13,4 +15,6 @@ module.exports = (app) => {
     app.use('/static', express.static('static'));  // всички заявки към static ще достъпват папката static
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());  //добавя cookieParser като midlleware
+    app.use(session());  //проверява дали текущата сесия е с активен потребител,т.е. дали има валидно cookie, ако няма го препраща към login 
+    app.use(trimBody('password'));
 };
