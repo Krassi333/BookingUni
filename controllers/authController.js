78adaa3698484257dfaa6124a82c198c49a0f340
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-04-30 22:33:29 +0300

    Add login in auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { register, login } = require('../services/userServices');
+const { parseError } = require('../util/parser');
 
 router.get('/register', (req, res) => {
     res.render('register', {
@@ -8,11 +9,33 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-    const token = await register(req.body.username, req.body.password);
+    try {
+        if (req.body.username == '' || req.body.password == '') {
+            throw new Error('All fields are required!');
+        }
 
-    res.cookie('token', token);
+        if (req.body.password != req.body.repass) {
+            throw new Error('Passwords don\'t match!');
+        }
+        //TODO check if register creates session 
+        const token = await register(req.body.username, req.body.password);
 
-    res.redirect('/auth/register');
+        res.cookie('token', token);
+
+        res.redirect('/');  //TODO replace with redirect by assignment
+    } catch (err) {
+
+        const errors = parseError(err);
+
+        //TODO Add error mesage to actual template 
+        res.render('register', {
+            title: "Register Page",
+            err,
+            body: {
+                username: req.body.username
+            }
+        })
+    }
 });
 
 router.get('/login', (req, res) => {
@@ -22,11 +45,23 @@ router.get('/login', (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-    const token = await login(req.body.username, req.body.password);
+    try {
+        const token = await login(req.body.username, req.body.password);
 
-    res.cookie('token', token);
+        res.cookie('token', token);
 
-    res.redirect('/auth/register');
+        res.redirect('/auth/register'); //TODO replace with redirect by assignment
+    } catch (err) {
+        const errors = parseError(err);
+
+        res.render('login', {
+            title: "Login Page",
+            errors,
+            body: {
+                username: req.body.username
+            }
+        })
+    }
 });
 
 module.exports = router;
