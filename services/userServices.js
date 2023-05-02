c546acfc1334247368f12f58393076467da8cabb
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-02 23:22:43 +0300

    Create user service

--- services/userServices.js
+++ services/userServices.js
@@ -4,15 +4,21 @@ const jwt = require('jsonwebtoken')
 
 const secret = 'erdfcv67gbl';
 
-async function register(username, password) {
-    const existing = await User.findOne({ username }).collation({ locale: 'en', strength: 2 });
-    if (existing) {
+async function register(email, username, password) {
+    const existingUsername = await User.findOne({ username }).collation({ locale: 'en', strength: 2 });
+    if (existingUsername) {
         throw new Error('Username is taken!');
+    }
+
+    const existingEmail = await User.findOne({ email }).collation({ locale: 'en', strength: 2 });
+    if (existingEmail) {
+        throw new Error('Email is taken!');
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await User.create({
+        email,
         username,
         hashedPassword
     });
@@ -22,17 +28,17 @@ async function register(username, password) {
     return token;
 }
 
-async function login(username, password) {
-    const user = await User.findOne({ username }).collation({ locale: 'en', strength: 2 });
+async function login(email, password) {
+    const user = await User.findOne({ email }).collation({ locale: 'en', strength: 2 });
 
     if (!user) {
-        throw new Error('Incorect username or password!');
+        throw new Error('Incorect email or password!');
     }
 
     const passwordCheck = await bcrypt.compare(password, user.hashedPassword);
 
     if (!passwordCheck) {
-        throw new Error('Incorect username or password!');
+        throw new Error('Incorect email or password!');
     }
 
     const token = createSesion(user);
@@ -47,9 +53,10 @@ function verifyToken(token) {
     return jwt.verify(token, secret);
 }
 
-function createSesion({ _id, username }) {
+function createSesion({ _id, email, username }) {
     const payload = {
         _id,
+        email,
         username
     }
 
