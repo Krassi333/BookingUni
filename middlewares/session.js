26aea67983b85a74c1f9946497a0d25037d0a215
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-02 23:21:02 +0300

    Create session middleware

--- middlewares/session.js
+++ middlewares/session.js
@@ -1,12 +1,13 @@
 const { verifyToken } = require("../services/userServices")
 
 module.exports = () => (req, res, next) => {
-    const token = req.cookies["token"];
-
+    const token = req.cookies.token;
+    //console.log("token: "+token);
     if (token) {
         try {
             const userData = verifyToken(token);
             req.user = userData;
+            res.locals.username = userData.username;
         } catch (err) {
             res.clearCookie('token');
             res.redirect('/auth/login');
