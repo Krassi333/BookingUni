9574a6706bea04d7b3bcc8028f3270b2213363d8
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-02 23:19:47 +0300

    Add routs

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,12 @@
 const homeController = require('../controllers/homeController');
 const authController = require('../controllers/authController');
+const prifileController = require('../controllers/profileController');
+const hotelController = require('../controllers/hotelController');
+const {hasUser}=require('../middlewares/guards');
 
 module.exports = (app) => {
     app.use('/', homeController);
     app.use('/auth', authController);
+    app.use('/profile', prifileController);
+    app.use('/hotel', hasUser(),hotelController);
 }
