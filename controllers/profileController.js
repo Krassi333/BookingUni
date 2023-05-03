3c6bb3e01c34d6835cc91cfcdcbd2a474c2e5266
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-03 17:17:26 +0300

    Create profile controller

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
+const { hasUser } = require('../middlewares/guards');
+const { getByUserBooking } = require('../services/hotelService');
 
-router.get('/', (req, res) => {
+router.get('/', hasUser(), async (req, res) => {
+    const bookings = await getByUserBooking(req.user.id);
+
     res.render('profile', {
-        title: "Profile Page"
+        title: "Profile Page",
+        user: Object.assign({ bookings }, req.user)
     })
 })
 
