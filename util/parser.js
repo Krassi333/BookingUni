6afc0ecb10e69676c3e05d4be12524ccb1a27c95
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-03 17:18:04 +0300

    Update utill parser

--- util/parser.js
+++ util/parser.js
@@ -1,5 +1,7 @@
 function parseError(error) {
+
     if (error.name == 'ValidationError') {
+        console.log(error);
         return Object.values(error.errors).map(v => v.message);
     } else {
         return error.message.split('\n');
